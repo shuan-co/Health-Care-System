f5a4f05895e85f277516985adf491cdf8d8a52a4
Gregorio John Marc A <122092610+1Cealus@users.noreply.github.com>
2023-10-07 12:33:35 +0800

    Changed clinic to be able to fetch data

--- client/src/pages/auth/clinic/Cdashboard.jsx
+++ client/src/pages/auth/clinic/Cdashboard.jsx
@@ -1,8 +1,26 @@
+import React, { useState } from 'react';
+import ClinicTest from './ClinicTest';
+
 function Cdashboard() {
+    const [clinicName, setClinicName] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
+
     return (
         <>
             <p>Clinic Logged In</p>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Clinic Name:
+                    <input type="text" value={clinicName} onChange={(e) => setClinicName(e.target.value)} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+            {clinicName && <ClinicTest clinicName={clinicName} />}
         </>
     );
 }
+
 export default Cdashboard;
