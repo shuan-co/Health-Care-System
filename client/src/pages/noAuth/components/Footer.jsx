1758cbe51b9a25ca656c44457cb16e0d1115a23d
Shuan Co <78131504+shuan-co@users.noreply.github.com>
2023-10-11 21:51:32 +0800

    Fixed Footer Position
    stick to bottom

--- client/src/pages/noAuth/components/Footer.jsx
+++ client/src/pages/noAuth/components/Footer.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className='w-screen bg-green-950 p-4'>
-        <p className="text-neutral-100 text-2xl lato">Contact us</p>
-            <p className='sans-serif text-neutral-100 text-base mt-2'>Health Center System</p>
-            <p className='sans-serif text-neutral-100 text-base'>contact@example.com</p>
-            <p className='sans-serif text-neutral-100 text-base'>Call us at: +1 (123) 456-7890.</p>
-            <p className='sans-serif text-neutral-100 text-base'>Visit us at: 123 Main Street, City, State, ZIP Code</p>
+    <footer className='w-screen bg-green-950 p-4 fixed bottom-0'>
+      <p className="text-neutral-100 text-2xl lato">Contact us</p>
+      <p className='sans-serif text-neutral-100 text-base mt-2'>Health Center System</p>
+      <p className='sans-serif text-neutral-100 text-base'>contact@example.com</p>
+      <p className='sans-serif text-neutral-100 text-base'>Call us at: +1 (123) 456-7890.</p>
+      <p className='sans-serif text-neutral-100 text-base'>Visit us at: 123 Main Street, City, State, ZIP Code</p>
     </footer>
   )
 }
