4de9dc50719a0a7b81415364ec4f80e73bf9e4ac
Shuan Co <78131504+shuan-co@users.noreply.github.com>
2023-12-04 23:25:34 +0800

    Reset Password and Search Date

--- client/src/pages/auth/patient/RecordDiagnoses.jsx
+++ client/src/pages/auth/patient/RecordDiagnoses.jsx
@@ -1,167 +1,165 @@
-import React, { useEffect, useState } from 'react'
-import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
 import { db, config } from '../../../firebase/Firebase';
 import { onAuthStateChanged } from 'firebase/auth';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
-const searchIcon = <FontAwesomeIcon icon={faMagnifyingGlass}/>
+const searchIcon = <FontAwesomeIcon icon={faMagnifyingGlass} />;
 
 export default function RecordDiagnoses() {
-    const [clinics, setClinics] = useState([])
-    const [diagnoses, setDiagnoses] = useState([])
-    const [showFullData, setShowFullData] = useState("")
-    const [index, setIndex] = useState(0)
+  const [clinics, setClinics] = useState([]);
+  const [diagnoses, setDiagnoses] = useState([]);
+  const [showFullData, setShowFullData] = useState(false);
+  const [index, setIndex] = useState(0);
+  const [searchDate, setSearchDate] = useState('');
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const user = await new Promise((resolve, reject) => {
-                    const unsubscribe = onAuthStateChanged(config.auth, user => {
-                        unsubscribe();
-                        if (user) {
-                            resolve(user);
-                        } else {
-                            reject(new Error('User not found'));
-                        }
-                    });
-                });
-    
-                const clinicsSnapshot = await getDocs(collection(db, "clinicPatient", user.uid, "clinics"));
-                const clinicsArray = clinicsSnapshot.docs.map(doc => doc.id);
-    
-                const diagnosesArray = [];
-                for (let i = 0; i < clinicsArray.length; i++) {
-                    const diagnosesSnapshot = await getDocs(collection(db, clinicsArray[i], "patients", "patientlist", user.uid, "diagnoses"));
-                    diagnosesSnapshot.forEach(doc => {
-                        diagnosesArray.push(doc.data());
-                    });
-                }
-    
-                setClinics(clinicsArray);
-                setDiagnoses(diagnosesArray);
-            } catch (error) {
-                console.error(error);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const user = await new Promise((resolve, reject) => {
+          const unsubscribe = onAuthStateChanged(config.auth, (user) => {
+            unsubscribe();
+            if (user) {
+              resolve(user);
+            } else {
+              reject(new Error('User not found'));
             }
-        }
-    
-        fetchData();
-    }, []);
-    
+          });
+        });
 
-    function showForm(index){
-        setShowFullData(true)
-        setIndex(index)
+        const clinicsSnapshot = await getDocs(collection(db, 'clinicPatient', user.uid, 'clinics'));
+        const clinicsArray = clinicsSnapshot.docs.map((doc) => doc.id);
+
+        const diagnosesArray = [];
+        for (let i = 0; i < clinicsArray.length; i++) {
+          const diagnosesSnapshot = await getDocs(
+            collection(db, clinicsArray[i], 'patients', 'patientlist', user.uid, 'diagnoses')
+          );
+          diagnosesSnapshot.forEach((doc) => {
+            diagnosesArray.push(doc.data());
+          });
+        }
+
+        setClinics(clinicsArray);
+        setDiagnoses(diagnosesArray);
+      } catch (error) {
+        console.error(error);
+      }
     }
+
+    fetchData();
+  }, []);
+
+  function showForm(index) {
+    setShowFullData(true);
+    setIndex(index);
+  }
+
+  // Filter diagnoses based on the entered date
+  const filteredDiagnoses = diagnoses.filter((diagnosis) =>
+    diagnosis.VisitDate.includes(searchDate)
+  );
 
   return (
     <div className='w-screen p-12'>
-        <div className='space-y-2 mb-8'>
-            <h1 className='lato text-2xl'>SEARCH RECORDS</h1>
-            <input type='text' className='border border-black rounded-lg w-2/4 h-8 p-2'></input>
-            <div className='ms-3 inline'>
-                <button>{searchIcon}</button>
-            </div>
-            {/*
-                <div className='inline ms-3 h-8'>
-                    <select
-                        id="sortBy"
-                        name="sortBy"
-                        autoComplete="sortBy"
-                        className="inline text-black block w-24 rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6 p-3"
-                    >
-                        <option selected>Sort by</option>
-                        <option>Entry</option>
-                        <option>Date</option>
-                    </select>
-                </div>
-                */
-            }   
+      <div className='space-y-2 mb-8'>
+        <h1 className='lato text-2xl'>SEARCH RECORDS</h1>
+        <input
+          type='text'
+          className='border border-black rounded-lg w-2/4 h-8 p-2'
+          placeholder='Search by Date (e.g., 2023-12-04)'
+          value={searchDate}
+          onChange={(e) => setSearchDate(e.target.value)}
+        ></input>
+        <div className='ms-3 inline'>
+          <button>{searchIcon}</button>
+        </div>
+      </div>
+
+      <div className='relative w-full lato'>
+        <div className='grid grid-cols-5 bg-blue-800 rounded-t-lg text-white exo text-center p-3'>
+          <h1>ENTRY</h1>
+          <h1>DATE</h1>
+          <h1>CHIEF COMPLAINT</h1>
+          <h1>CLINIC</h1>
+          <h1>OUTPATIENT FORM</h1>
         </div>
 
-        <div className='relative w-full lato'>
-            <div className='grid grid-cols-5 bg-blue-800 rounded-t-lg text-white exo text-center p-3'>
-                <h1>ENTRY</h1>
-                <h1>DATE</h1>
-                <h1>CHIEF COMPLAINT</h1>
-                <h1>CLINIC</h1>
-                <h1>OUTPATIENT FORM</h1>
+        <div className='bg-slate-100 rounded-b-lg'>
+          {filteredDiagnoses.map((diagnosis, index) => (
+            <div className='grid grid-cols-5 text-center p-4 border border-black' key={index}>
+              <h3 className='font-bold'>{index + 1}</h3>
+              <h3 className='border bg-purple-200 rounded-2xl'>{diagnosis.VisitDate}</h3>
+              <h3 className='capitalize'>{diagnosis.ChiefComplaint}</h3>
+              <h3 className='capitalize'>{diagnosis.Clinic}</h3>
+              <button
+                onClick={() => showForm(index)}
+                className='hover:bg-slate-200 rounded-lg border border-black enriqueta'
+              >
+                View Form
+              </button>
             </div>
-            
-            <div className='bg-slate-100 rounded-b-lg'>
-                {diagnoses.map((diagnosis, index) => (
-                    <div className='grid grid-cols-5 text-center p-4 border border-black'>
-                        <h3 className='font-bold'>{index + 1}</h3>
-                        <h3 className='border bg-purple-200 rounded-2xl'>{diagnosis.VisitDate}</h3>
-                        <h3 className='capitalize'>{diagnosis.ChiefComplaint}</h3>
-                        <h3 className='capitalize'>{diagnosis.Clinic}</h3>
-                        <button onClick={() => showForm(index)} className='hover:bg-slate-200 rounded-lg border border-black enriqueta'>View Form</button>
-                    </div>
-                ))}
-                <div className='w-full h-10'>
-
-                </div>
-                
-            </div>
-            
-
+          ))}
+          <div className='w-full h-10'></div>
         </div>
+      </div>
 
-        {showFullData && (
-            <div className="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                <div className="relative w-full max-w-2xl max-h-full mx-auto lato">
-                    <div className="flex items-start justify-between p-4 border-b rounded-t bg-blue-800">
-                        <h3 className="p-2 text-xl font-semibold text-white-900 dark:text-white">
-                            Diagnosis
-                        </h3>
-                        <button
-                            className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-slate-50 dark:hover:text-black"
-                            onClick={() => setShowFullData(false)}
-                        >
-                            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                            </svg>
-                            <span className="sr-only">Close modal</span>
-                        </button>
-                    </div>
-                    <div className="relative bg-white shadow shadow-2xl drop-shadow-2xl border border-black">
-                        <div className='p-6 grid grid-cols-2'>
-                            <h3 className='font-bold'>Clinic</h3>
-                            <p>{diagnoses[index].Clinic}</p>
-                            <h3 className='font-bold'>Chief Complaint</h3>
-                            <p>{diagnoses[index].ChiefComplaint}</p>
-                            <h3 className='font-bold'>Assessment</h3>
-                            <p>{diagnoses[index].Assessment}</p>
-                            <h3 className='font-bold'>Date</h3>
-                            <p>{diagnoses[index].VisitDate}</p>
-                            <h3 className='font-bold'>Follow Up Date</h3>
-                            <p>{diagnoses[index].FollowUpDate}</p>
-                            <h3 className='font-bold'>Disposition</h3>
-                            <p>{diagnoses[index].Disposition}</p>
-                            <h3 className='font-bold'>Treatment</h3>
-                            <p>{diagnoses[index].Treatment}</p>
-                            <h3 className='font-bold'>Blood Pressure</h3>
-                            <p>{diagnoses[index].BP}</p>
-                            <h3 className='font-bold'>Heart Rate</h3>
-                            <p>{diagnoses[index].HeartRate}</p>
-                            <h3 className='font-bold'>Respiratory Rate</h3>
-                            <p>{diagnoses[index].RespiratoryRate}</p>
-                            <h3 className='font-bold'>Height</h3>
-                            <p>{diagnoses[index].Height}</p>
-                            <h3 className='font-bold'>Weight</h3>
-                            <p>{diagnoses[index].Weight}</p>
-                            <h3 className='font-bold'>Last Menstration Date</h3>
-                            <p>{diagnoses[index].LastMensDate}</p>
-                            <h3 className='font-bold'>Temperature</h3>
-                            <p>{diagnoses[index].Temperature}</p>
-                            <h3 className='font-bold'>Notes:</h3>
-                            <p>{diagnoses[index].ProviderNotes}</p>
-                        </div>
-                    </div>
-                
-                </div>
-            </div>
-        )}
+          {showFullData && (
+              <div className="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                  <div className="relative w-full max-w-2xl max-h-full mx-auto lato">
+                      <div className="flex items-start justify-between p-4 border-b rounded-t bg-blue-800">
+                          <h3 className="p-2 text-xl font-semibold text-white-900 dark:text-white">
+                              Diagnosis
+                          </h3>
+                          <button
+                              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-slate-50 dark:hover:text-black"
+                              onClick={() => setShowFullData(false)}
+                          >
+                              <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                              </svg>
+                              <span className="sr-only">Close modal</span>
+                          </button>
+                      </div>
+                      <div className="relative bg-white shadow shadow-2xl drop-shadow-2xl border border-black">
+                          <div className='p-6 grid grid-cols-2'>
+                              <h3 className='font-bold'>Clinic</h3>
+                              <p>{diagnoses[index].Clinic}</p>
+                              <h3 className='font-bold'>Chief Complaint</h3>
+                              <p>{diagnoses[index].ChiefComplaint}</p>
+                              <h3 className='font-bold'>Assessment</h3>
+                              <p>{diagnoses[index].Assessment}</p>
+                              <h3 className='font-bold'>Date</h3>
+                              <p>{diagnoses[index].VisitDate}</p>
+                              <h3 className='font-bold'>Follow Up Date</h3>
+                              <p>{diagnoses[index].FollowUpDate}</p>
+                              <h3 className='font-bold'>Disposition</h3>
+                              <p>{diagnoses[index].Disposition}</p>
+                              <h3 className='font-bold'>Treatment</h3>
+                              <p>{diagnoses[index].Treatment}</p>
+                              <h3 className='font-bold'>Blood Pressure</h3>
+                              <p>{diagnoses[index].BP}</p>
+                              <h3 className='font-bold'>Heart Rate</h3>
+                              <p>{diagnoses[index].HeartRate}</p>
+                              <h3 className='font-bold'>Respiratory Rate</h3>
+                              <p>{diagnoses[index].RespiratoryRate}</p>
+                              <h3 className='font-bold'>Height</h3>
+                              <p>{diagnoses[index].Height}</p>
+                              <h3 className='font-bold'>Weight</h3>
+                              <p>{diagnoses[index].Weight}</p>
+                              <h3 className='font-bold'>Last Menstration Date</h3>
+                              <p>{diagnoses[index].LastMensDate}</p>
+                              <h3 className='font-bold'>Temperature</h3>
+                              <p>{diagnoses[index].Temperature}</p>
+                              <h3 className='font-bold'>Notes:</h3>
+                              <p>{diagnoses[index].ProviderNotes}</p>
+                          </div>
+                      </div>
+
+                  </div>
+              </div>
+          )}
     </div>
-  )
+  );
 }
--- client/src/pages/noAuth/login/Login.jsx
+++ client/src/pages/noAuth/login/Login.jsx
@@ -1,15 +1,35 @@
-// Firebase
-import { setPersistence, signInWithEmailAndPassword, inMemoryPersistence, onAuthStateChanged } from "firebase/auth";
+// Import necessary Firebase authentication functions
+import { setPersistence, signInWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
 import { config, user } from "../../../firebase/Firebase";
 
-//  Routing
-import { useNavigate } from 'react-router-dom';
-
-// Context
-import { useContext } from 'react';
+// Import React dependencies
+import React, { useContext, useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 import { AuthContext } from '../../../AuthContext';
 
+import { Fragment, useRef } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
+
 function Login() {
+    const [open, setOpen] = useState(false)
+
+    const cancelButtonRef = useRef(null)
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [resetSent, setResetSent] = useState(false);
+
+    const handleReset = async (event) => {
+        event.preventDefault();
+
+        try {
+            await sendPasswordResetEmail(config.auth, email);
+            setResetSent(true);
+            alert('Email has been sent');
+        } catch (error) {
+            console.error(error.message);
+        }
+    };
     const navigate = useNavigate();
     const { setIsLoggedIn } = useContext(AuthContext);
     const authenticate = (event) => {
@@ -55,6 +75,92 @@ function Login() {
 
     return (
         <div id="loginPage" className="flex justify-center items-center h-screen w-screen">
+            <Transition.Root show={open} as={Fragment}>
+                <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                    </Transition.Child>
+
+                    <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            >
+                                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                                    <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                                        <div className="sm:flex sm:items-start">
+                                            <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                                <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                                            </div>
+                                            <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                                                <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                                                    Reset your Password!
+                                                </Dialog.Title>
+                                                <div className="mt-2">
+                                                    <p className="text-sm text-gray-500">
+                                                        Assuming you enter a verified email address, an email will be sent to you containing a link to reset your password.
+                                                    </p>
+                                                    <br />
+                                                    {resetSent ? (
+                                                        <p>Password reset email sent. Check your inbox.</p>
+                                                    ) : (
+                                                        <form onSubmit={handleReset}>
+                                                            <label htmlFor="email">Enter your email:</label>
+                                                            <input
+                                                                type="email"
+                                                                id="email"
+                                                                value={email}
+                                                                onChange={(e) => setEmail(e.target.value)}
+                                                                style={{marginLeft:"20px"}}
+                                                                placeholder="Enter your Email Address"
+                                                            />
+                                                        </form>
+                                                    )}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                        <button
+                                            type="button"
+                                            className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                                            onClick={(event) => {
+                                                setOpen(false);
+                                                handleReset(event);
+                                            }}
+                                            style={{ marginLeft: "20px" }}
+                                        >
+                                            Send Reset Password Link
+                                        </button>
+                                        <button
+                                            type="button"
+                                            className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                                            onClick={() => setOpen(false)}
+                                            ref={cancelButtonRef}
+                                        >
+                                            Cancel
+                                        </button>
+                                    </div>
+                                </Dialog.Panel>
+                            </Transition.Child>
+                        </div>
+                    </div>
+                </Dialog>
+            </Transition.Root>
             <div className="w-screen h-3/4">
                 <div className="mb-5">
                     <h1 className="enriqueta text-center text-2xl">Health Center System</h1>
@@ -102,7 +208,7 @@ function Login() {
                             text-blue-600
                             text-center 
                             mb-5
-                            hover:underline" href="">Forgot Password?</a>
+                            hover:underline" onClick={() => {setOpen(true)}}>Reset / Forgot Password?</a>
                     <button onClick={authenticate}
                         className="rounded-full 
                             bg-blue-700
@@ -114,7 +220,6 @@ function Login() {
                             hover:bg-blue-600">Sign in</button>
                 </form>
             </div>
-            
         </div>
     );
 }
